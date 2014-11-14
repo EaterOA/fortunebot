1398a0c014784712c0e48a7878031bdac7fa17a6
Vincent Wong <duperduper@ucla.edu>
2014-11-14 01:04:29 -0800

    Added burn script The burn script just returns a random choice amongst a bunch of TOLD variants whenever someone posts public message that contains all of the magic words set in the config file.

---
+++ fortunebot/scripts/burn.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+burn.py
+"""
+
+import random
+
+class Burn(object):
+
+    NAME = "burn"
+    PARAMS = [("str", "words")]
+    HELP = "Burn"
+
+    def __init__(self, words):
+        self.words = words.split()
+
+    def on_pubmsg(self, nick, channel, text):
+        text = text.lower()
+        for w in self.words:
+            d = text.find(w)
+            if d == -1:
+                return None
+            text = text[d+1:]
+        return self.get_burned()
+
+    def get_burned(self):
+        choices = [
+            "TOLD",
+            "TOLDASAURUS REX",
+            "Cash4TOLD.com",
+            "Knights of the TOLD Republic",
+            "King TOLD the IV",
+            "CounTOLD Strike",
+            "Unreal TOLDament",
+            "Rampage: TOLDal Destruction",
+            "The Good, The Bad, and The TOLD",
+            "Super TOLD Boy",
+            "Left 4 TOLD",
+            "TOLDman Sachs",
+            "TOLD Fortress 2",
+            "www.TOLD.com",
+            "Tic-tac-TOLD",
+            "TOLD of the Ring",
+        ]
+        return random.choice(choices)
