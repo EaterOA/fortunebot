aa15b8af4bbd31a015628cabb2194a2e130ea6a6
Vincent Wong <duperduper@ucla.edu>
2015-02-14 21:41:50 -0800

    EasyConfigParser's get functions now allow default values

--- fortunebot/utils/easyconfigparser.py
+++ fortunebot/utils/easyconfigparser.py
@@ -22,3 +22,29 @@ class EasyConfigParser(ConfigParser.RawConfigParser):
         for s in sections:
             self.add_section(s)
 
+    def defaultable(f):
+        def wrapper(self, section, option, default=None):
+            try:
+                return f(self, section, option)
+            except:
+                if default == None:
+                    raise
+                else:
+                    return default
+        return wrapper    
+
+    @defaultable
+    def get(self, section, option):
+        return ConfigParser.RawConfigParser.get(self, section, option)
+
+    @defaultable
+    def getboolean(self, section, option):
+        return ConfigParser.RawConfigParser.getboolean(self, section, option)
+
+    @defaultable
+    def getint(self, section, option):
+        return ConfigParser.RawConfigParser.getint(self, section, option)
+
+    @defaultable
+    def getfloat(self, section, option):
+        return ConfigParser.RawConfigParser.getfloat(self, section, option)
