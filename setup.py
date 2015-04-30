09b3e9668935bb14fb733f372e29db5ec20c5841
Vincent Wong <duperduper@ucla.edu>
2015-04-30 01:51:15 -0700

    Replaced install with setuptools console_script
    fortunebot no longer manually creates an /etc/fortunebot nor installs init
    scripts. It *really* doesn't need to be a init service.

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup
 
-setup(name='fortunebot',
-      version='1.1',
-      description="IRC bot for UCLA LUG",
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 2.7',
-      ],
-      url="https://github.com/EaterOA/fortunebot",
-      author="Vincent Wong",
-      author_email="duperduper@ucla.edu",
-      license="GPL3",
-      packages=["fortunebot", "fortunebot.scripts", "fortunebot.utils"],
-      install_requires=["irc>=11", "importlib"],
-      zip_safe=False
-     )
+setup(
+    name='fortunebot',
+    version='1.1',
+    description="IRC bot for UCLA LUG",
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Programming Language :: Python :: 2.7',
+    ],
+    url="https://github.com/EaterOA/fortunebot",
+    author="Vincent Wong",
+    author_email="duperduper@ucla.edu",
+    license="GPL3",
+    packages=["fortunebot", "fortunebot.scripts", "fortunebot.utils"],
+    install_requires=["irc>=11", "importlib"],
+    entry_points={
+        'console_scripts': ['fortunebot = fortunebot.botrunner:main'],
+    },
+)
